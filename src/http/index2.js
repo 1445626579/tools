65a2e68c897060e86945466727f7634d4d264c3e
1445626579 <40221753+1445626579@users.noreply.github.com>
2021-02-01 12:02:10 +0800

    Update index2.js

--- src/http/index2.js
+++ src/http/index2.js
@@ -1,167 +1,131 @@
 import Axios from "axios";
 import qs from "qs";
-import initCodes from "./code.config";
-import toast from '../utils/toast'
-import { promiseLoading, promiseDebounce } from "../utils"
-//code数组合并
-function codeArrayConcat (initCodes, codes) {
-  const resultArr = [].concat(initCodes)
-  codes.forEach((item) => {
-    const searchRes = initCodes.findIndex((initItem) => initItem.code === item.code)
-    if (searchRes===-1) {
-      resultArr.push(item)
-    }else{
-      resultArr[searchRes]=item
-    }
-  })
-  return resultArr
+import { Toast } from "vant"
+import { promiseDebounce, promiseLoading } from "./index"
+const customToast = (txt) => {
+  Toast(
+    {
+      message:txt,
+      icon: require('@/assets/imgs/fail.svg'),
+      className: 'customStyle'
+    })
 }
-//遍历的响应函数
-const eachHandle = (item, isUserVisible, toast,data) => {
-  const {code,message:msg}=data
-  const output = (text) => {
-    if (isUserVisible) {
-      toast(text)
-    } else {
-      if(typeof item.handle==="function"){
-        item.handle(data)
-        return
-      }
-      console.error(text);
-    }
-  };
-  if (typeof item.code === "function") {
-    if (item.code(code)) {
-      output(msg);
-      return true;
-    }
-  } else if (code === item.code) {
-    output(item.tips || msg);
-    return true;
-  }
-};
-export default function (config = {}, apis = {}) {
-  const defaults = {
+class Http{
+  //默认设置
+  config = {
     baseURL: "", //根地址
     timeout: 10 * 1000, //超时时间
-    getToken: () => true, //获取token的函数返回值为token
+    getToken: () => false, //获取token的函数返回值为token
     setToken: () => { }, //在请求中设置token，如果没有从getToken中获取到token则不会执行
     notLoginHandle: () => { }, //如果未登录的响应函数
     customInterceptors: false, //自定义请求拦截
-    codes: {},//code的扩展
+    codes: [],//code的扩展
     requestType: "json",
-    loading: false,
+    loading:promiseLoading,
+    showLoading: false,
     debounce: false,
-    method: "post",
-    noNeedToken: false,
-    commonData: {},//公共data
-    toast
-  };
-  const setting = Object.assign({}, defaults, config)
-  const { userVisibleCodes = [], otherCodes = [] } = setting.codes;
-
-  setting.codes = {
-    ...setting.codes,
-    userVisibleCodes: codeArrayConcat(initCodes.userVisibleCodes, userVisibleCodes),
-    otherCodes: codeArrayConcat(initCodes.otherCodes, otherCodes)
+    method: 'get',
+    unwantedToken: false,
+    commonData: {},//公共data,可选值为函数参数为config
+    toast:customToast
   }
-  const axios = Axios.create(setting);
-  //请求拦截器
-  axios.interceptors.request.use(
-    function (config) {
-      //判断是否有token
-      const token = typeof config.getToken === "function" ? config.getToken() : false;
-      if (token) {
-        if (typeof config.setToken === "function") config.setToken(config, token);
-      } else if (!config.noNeedToken) {
-        const source = Axios.CancelToken.source();
-        config.cancelToken = source.token;
-        if (typeof config.notLoginHandle === "function") {
-          config.notLoginHandle();
-        }
-        source.cancel("用户未登录")
-        return Promise.reject("用户未登录");
-      }
-      //如果请求类型是否为form表单类型
-      if (config.requestType === "form") {
-        config.headers["Content-Type"] = "application/x-www-form-urlencoded";
-        config.data = qs.stringify(config.data);
-      } else if (config.requestType === "upFile") {
-        //是否为文件上传
-        config.headers["Content-Type"] = "multipart/form-data";
-        const formData = new FormData();
-        Object.keys(config.data).forEach((key) => {
-          formData.append(key, config.data[key]);
-        });
-        config.data = formData;
-      }
-      if (typeof config.beforeSend === "function") {
-        config = config.beforeSend(config);
-      }
-      return config;
-    },
-    function (error) {
-      // 对请求错误做些什么
-      return Promise.reject(error);
+  //数据转换为表单格式
+  dataToFormFormat(data) {
+    const formData = new FormData();
+    Object.keys(data).forEach((key) => {
+      formData.append(key, data[key]);
+    });
+    return formData
+  }
+  // 未登录的响应函数
+  notLoginHandle(config) {
+    //判断是否有token,有token时调用setToken，设置到请求中
+    const token = typeof config.getToken === "function" ? config.getToken() : false;
+    if (token) {
+      if (typeof config.setToken === "function") config.setToken(config, token);
     }
-  );
-
-  // 添加响应拦截器
-  axios.interceptors.response.use(
-    function (response) {
-      if (typeof response.config.customInterceptors === "function") {
-        return response.config.customInterceptors(response);
+    
+    else if (!config.unwantedToken) {
+      const source = Axios.CancelToken.source();
+      config.cancelToken = source.token;
+      if (typeof config.notLoginHandle === "function") {
+        config.notLoginHandle();
       }
-      const { code, message: msg = "未知错误" } = response.data;
-      const { codes } = response.config
-      console.log(codes)
-      if (code !== 200) {
-        /**
-         * 如果在定义的code码中找到，以定义的code码对应提示信息提示，如果不存在则以接口返回信息提示
-         * 如果没有在提示用户的code与otherCode中找到，则以接口的提示信息进行提示，用户不可见
-         */
-        //是否在用户可见信息内找到
-        if (code === undefined) return Promise.reject(msg);
-        const isFind = codes.userVisibleCodes.some((item) =>
-          eachHandle(item,  true, response.config.toast,response.data)
-        );
-        if (!isFind) {
-          const isOtherFind = codes.otherCodes.some((item) =>
-            eachHandle(item, false, response.config.toast,response.data)
-          );
-          if (!isOtherFind) {
-            return Promise.reject(response.data);
-          }
-        }
-        return Promise.reject(response.data);
-      }
-      return response.data;
-    },
-    function (error) {
-      // 对响应错误做点什么
-      return Promise.reject(error);
+      source.cancel("用户未登录")
     }
-  );
-  /**
-     * 1.将请求接口写到apis中，将自动生成一个对应AJAX请求函数
-     * 2.函数包含两个参数第一个为Data值，第二个为对请求的设置config对象
-     * @param {Object} data 请求参数设置
-     * @param {Object} config 请求设置，包括请求类型、loading和防抖
-     * 3.config属性设置
-     * {
-     *  requestType:'json || form || upFile' 默认请求类型为json，form为表单方式提交，upFile为文件上传
-     *  loading:false || true 是否需要loading
-     *  debounce: false || true 是否需要防抖
-     *  method:'post' 请求类型 默认post
-     *  customPrefixed:function 自定义前置函数，函数接收axios请求函数，返回值必须是一个返回值为promise的函数,可接收一个数组，值为多个前置函数
-     *  beforeSend:function 请求发送之前，参数为config  function(config){config.xx=xx; return config}
-     * }
-     */
-  const request = (url = '', data = {}, config = {}) => {
-    const { loading, ...restConfig } = config
+  }
+  // 请求类型非json时
+  setRequsetType(config) {
+    //如果请求类型是否为form表单类型
+    if (config.requestType === "form") {
+      config.headers["Content-Type"] = "application/x-www-form-urlencoded";
+      config.headers["Access-Control-Allow-Origin"] = "*";
+      config.data = qs.stringify(config.data);
+    }
+    else if (config.requestType === "upFile") {
+      //是否为文件上传
+      config.headers["Content-Type"] = "multipart/form-data";
+      config.data = this.dataToFormFormat(config.data);
+    }
+  }
+  //请求发送前的拦截器
+  interceptor(config) {
+    const _this = this;
+    return {
+      beforeRequest (axiosConfig) {
+        _this.notLoginHandle(axiosConfig)
+        _this.setRequsetType(axiosConfig)
+        if (typeof axiosConfig.beforeSend === "function") {
+          axiosConfig = axiosConfig.beforeSend(axiosConfig);
+        }
+        return axiosConfig;
+      },
+      beforeError(error) {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+      },
+      //响应的拦截
+      response(response) {
+        if (typeof config.customInterceptors === "function") {
+          return config.customInterceptors(response);
+        }
+        const { code, message: msg = "未知错误" } = response.data;
+        const { codes } = config
+        if (code !== 200) {
+          //是否在用户可见信息内找到
+          if (code === undefined) return Promise.reject(msg);
+          codes.some((item) =>{
+            const { data } = response
+            if ((typeof item.code === "function" && item.code(code)) || code === item.code) {
+              if (item.visible) {
+                config.toast(msg, response)
+              }
+              if(typeof item.handle === "function") {
+                item.handle(data)
+                return true
+              }
+              if (!item.visible&&!item.handle) {
+                console.error(msg);
+              }
+              return true;
+            } 
+          });
+          return Promise.reject(response.data);
+        }
+        return response.data;
+      },
+      // 响应错误的拦截
+      responseError (err) {
+        // 对响应错误做点什么
+        return Promise.reject(err);
+      },
+    }
+  }
+  request (url, data, config) {
+    const { showLoading, ...restConfig } = config
     //前置函数列表
     const prefixedHandle = []
-    if (loading) prefixedHandle.push(promiseLoading)
+    if (showLoading) prefixedHandle.push(this.config.loading)
     if (typeof config.customPrefixed !== "undefined") {
       if (typeof config.customPrefixed === "function") {
         prefixedHandle.push(config.customPrefixed)
@@ -169,35 +133,53 @@ export default function (config = {}, apis = {}) {
         prefixedHandle.push(...config.customPrefixed.filter(item => typeof item === 'function'))
       }
     }
-    const http = prefixedHandle.length > 1 ? prefixedHandle.reduce((prev, next) => next(prev(axios))) :
-      prefixedHandle.length === 1 ? prefixedHandle[0](axios) : axios
+    const http = prefixedHandle.length > 1 ? prefixedHandle.reduce((prev, next) => next(prev(this.axios))) :
+      prefixedHandle.length === 1 ? prefixedHandle[0](this.axios) : this.axios
     return http({ [restConfig.method === "post" ? 'data' : 'params']: data, url, ...restConfig })
   }
-  if (typeof apis === "object" && apis) {
-    /**
-     * 如果该请求需要防抖，把加了防抖的方法放入debounceObj，第二次调用时返回该方法
-     * 该方法再次发起请求时
-     */
-    const debounceList = {}
-    Object.keys(apis).forEach((key) => {
-      this[key] = (data={},config = {}) => {
-        // 当第一个参数为显式声明为config时，将data对象置为空
-        if(data.isConfig){
-          config=JSON.parse(JSON.stringify(data));
-          data={}
-        }
-        config = { ...setting, ...config }
-        const params = { ...config.commonData, ...data }
-        if (config.debounce) {
-          if (!debounceList[key]) {
-            debounceList[key] = promiseDebounce(request)
+  createApis(apis,setting={}) {
+    if (typeof apis === "object" && apis) {
+      /**
+       * 如果该请求需要防抖，把加了防抖的方法放入debounceObj，第二次调用时返回该方法
+       */
+      const debounceList = {}
+      return Object.keys(apis).reduce((api, key) => {
+        api[key] = (data={},config = {}) => {
+          // 当第一个参数为显式声明为config时，将data对象置为空
+          if(data.isConfig){
+            config=JSON.parse(JSON.stringify(data));
+            data = {}
           }
-          return debounceList[key](apis[key], params, config)
+          config = { ...setting, ...config }
+          //参数融合放在请求中
+          const commonData = typeof config.commonData === 'function' ? config.commonData(config) : config.commonData;
+          const params = { ...commonData, ...data }
+          if (config.debounce) {
+            if (!debounceList[key]) {
+              debounceList[key] = promiseDebounce(this.request)
+            }
+            return debounceList[key].call(this,apis[key], params, config)
+          }
+          return this.request(apis[key], params, config);
         }
-        delete config.commonData
-        delete config.debounce
-        return request(apis[key], params, config);
-      };
-    });
+        return api
+      },{})
+    }
   }
+  init(apis,config) {
+    this.config = Object.assign({}, this.config, config);
+    //this.config.codes = this.codeArrayConcat()
+    // 创建axios对象
+    this.axios = Axios.create(this.config);
+    const { beforeRequest, beforeError ,response,responseError} = this.interceptor(this.config)
+    // 设置拦截器
+    this.axios.interceptors.request.use(beforeRequest,beforeError)
+    this.axios.interceptors.response.use(response, responseError)
+    return this.createApis(apis,config)
+  }
+}
+export default function (config,apis={}) {
+  const http = new Http
+  const api = http.init(apis, config)
+  Object.assign(this,api)
 }
